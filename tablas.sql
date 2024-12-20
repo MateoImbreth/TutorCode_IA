c50a9a6fa943e67ae5f455a1948087556c0feac5
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-19 19:22:37 -0500

    Mejoras en la definición de tablas: se corrigieron convenciones de estilo y se añadió la tabla EjerciciosResueltos.

--- tablas.sql
+++ tablas.sql
@@ -1,33 +1,35 @@
-Create table Usuario(
-	id SERIAL primary key,
+CREATE TABLE Usuario (
+	id SERIAL PRIMARY KEY,
 	nombre VARCHAR(100) NOT NULL,
 	email VARCHAR(150) NOT NULL UNIQUE,
 	contrasena VARCHAR(100) NOT NULL,
 	rol VARCHAR(50) NOT NULL
 );
 
-Create table EjerciciosResueltos(
-	id SERIAL primary key,
-    CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES Usuario (id) ON DELETE CASCADE,
-    CONSTRAINT fk_ejercicio FOREIGN KEY (ejercicio_id) REFERENCES Ejercicio (id) ON DELETE CASCADE
-);
-
-Create table Ejercicio(
-	id SERIAL primary key,
+CREATE TABLE Ejercicio (
+	id SERIAL PRIMARY KEY,
 	titulo VARCHAR(200) NOT NULL,
-	contenido text NOT NULL,
+	contenido TEXT NOT NULL,
 	nivel_dificultad VARCHAR(50) NOT NULL,
 	lenguaje_pro VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Progreso (
-    id SERIAL PRIMARY KEY,
-    usuario_id INT NOT NULL,
-    ejercicio_id INT NOT NULL,
-    resultado INT NOT NULL,
-    fecha DATE NOT NULL DEFAULT CURRENT_DATE,
-    CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES Usuario (id) ON DELETE CASCADE,
-    CONSTRAINT fk_ejercicio FOREIGN KEY (ejercicio_id) REFERENCES Ejercicio (id) ON DELETE CASCADE
+	id SERIAL PRIMARY KEY,
+	usuario_id INT NOT NULL,
+	ejercicio_id INT NOT NULL,
+	resultado INT NOT NULL,
+	fecha DATE NOT NULL DEFAULT CURRENT_DATE,
+	CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES Usuario (id) ON DELETE CASCADE,
+	CONSTRAINT fk_ejercicio FOREIGN KEY (ejercicio_id) REFERENCES Ejercicio (id) ON DELETE CASCADE
+);
+
+CREATE TABLE EjerciciosResueltos (
+	id SERIAL PRIMARY KEY,
+	usuario_id INT NOT NULL,
+	ejercicio_id INT NOT NULL,
+	CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES Usuario (id) ON DELETE CASCADE,
+	CONSTRAINT fk_ejercicio FOREIGN KEY (ejercicio_id) REFERENCES Ejercicio (id) ON DELETE CASCADE
 );
 
 INSERT INTO usuario (nombre, email, contrasena, rol)
