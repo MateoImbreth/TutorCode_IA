816d6241640dfd1121a33fd0fbfae56b6d45e576
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-19 22:12:17 -0500

    Se añadió configuración de CORS en la aplicación FastAPI para permitir solicitudes de cualquier origen.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine
 from models import Base
 from routes import user_routes, languaje_routes, exercise_routes, progress_routes, editor_routes, resolvedex_routes
 
 # Inicializar la aplicación FastAPI
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las solicitudes de origen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Crear las tablas en la base de datos
 Base.metadata.create_all(bind=engine)
